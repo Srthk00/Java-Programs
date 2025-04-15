8cc9604c4bd31d876b684642b9616d211f9f7916
Sarthak <sarthakkothiyal83@gmail.com>
2025-04-15 17:26:17 +0530

    codes

--- TermWork/permutation.java
+++ TermWork/permutation.java
@@ -1,8 +1,28 @@
 import java.util.*;
 
 public class permutation{
+    static void swap(StringBuffer str,int a,int b){
+        char ch=str.charAt(a);
+        str.setCharAt(a,str.charAt(b));
+        str.setCharAt(b,ch);
+    }
+
+    static void permu(StringBuffer str,int low,int high){
+        if(low==high){
+            System.out.println(str);
+            return;
+        }
+        for(int i=low;i<=high;i++){
+            swap(str,low,i);
+            permu(str,low+1,high);
+            swap(str,low,i);
+        }
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a String: ");
         String str=sc.nextLine();
+        StringBuffer s=new StringBuffer(str);
+        permu(s,0,str.length()-1);
     }
 }
